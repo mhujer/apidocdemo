2426d35754eb240abefd468cca4cb47eb15e0349
Martin Hujer <mhujer@gmail.com>
2014-06-08 14:41:13 +0200

    ApiDoc Bundle setup

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new FOS\RestBundle\FOSRestBundle(),
             new JMS\SerializerBundle\JMSSerializerBundle(),
+            new Nelmio\ApiDocBundle\NelmioApiDocBundle(),
             new Mhujer\ApiDemoBundle\MhujerApiDemoBundle(),
         );
 
--- src/Mhujer/ApiDemoBundle/Controller/GistsController.php
+++ src/Mhujer/ApiDemoBundle/Controller/GistsController.php
@@ -3,12 +3,35 @@
 namespace Mhujer\ApiDemoBundle\Controller;
 
 use FOS\RestBundle\Controller\FOSRestController;
+use Nelmio\ApiDocBundle\Annotation\ApiDoc;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
 class GistsController extends FOSRestController
 {
+    /**
+     * List the authenticated user’s gists or if called anonymously, this will return all public gists:
+     *
+     * @ApiDoc(
+     *      parameters={
+     *          {
+     *              "name"="since",
+     *              "dataType"="string",
+     *              "required"=false,
+     *              "description"="A timestamp in ISO 8601 format: YYYY-MM-DDTHH:MM:SSZ. Only gists updated at or after this time are returned."
+     *          }
+     *      }
+     * )
+     */
     public function getGistsAction()
     {
-        return new JsonResponse(['list' => 'foo']);
+        return new JsonResponse(
+            json_decode(file_get_contents(__DIR__ . '/../Data/Gists/getGists.json')),
+            200,
+            [
+                'Link' => '<https://api.github.com/resource?page=2>; rel="next", <https://api.github.com/resource?page=5>; rel="last"',
+                'X-RateLimit-Limit' => 5000,
+                'X-RateLimit-Remaining' => 4999
+            ]
+        );
     }
 }
